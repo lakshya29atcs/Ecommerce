e555480af954dbedbd15b512b7724ae2ab3378e3
Aditi Rathore <aditi.rathore@atcs.com>
2022-06-28 14:49:16 +0530

    order and orderItem

--- EcommerceWeb/src/main/java/com/atcs/ecommerce/entity/OrderItem.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/entity/OrderItem.java
@@ -1,5 +1,102 @@
 package com.atcs.ecommerce.entity;
 
-public class OrderItem {
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="orderItem")
+public class OrderItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private @NotNull int quantity;
+	
+	@Column
+	private @NotNull double price;
+	
+	@Column 
+	private Date createdDate;
+	
+	@ManyToOne
+	private Order order;
+	
+	@OneToOne
+	private Product product;
+
+	public OrderItem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderItem(int id, int quantity, double price, Date createdDate, Order order, Product product) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.price = price;
+		this.createdDate = createdDate;
+		this.order = order;
+		this.product = product;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 }

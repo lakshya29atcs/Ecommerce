f27550b3623ec2417d98cb26255cb0337ceade66
Aditi Rathore <aditi.rathore@atcs.com>
2022-06-28 17:27:14 +0530

    service implementation

--- EcommerceWeb/src/main/java/com/atcs/ecommerce/service/OrderItemService.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/service/OrderItemService.java
@@ -1,5 +1,17 @@
 package com.atcs.ecommerce.service;
 
-public class OrderItemService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.atcs.ecommerce.entity.OrderItem;
+
+@Service
+public interface OrderItemService {
+	public void post(OrderItem or);
+	public void put(OrderItem or);
+	public OrderItem getById(int id);
+	public List<OrderItem> getAll();
+	public void delete(int id);
+	
 }
--- EcommerceWeb/src/main/java/com/atcs/ecommerce/serviceimpl/OrderServiceImpl.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/serviceimpl/OrderServiceImpl.java
@@ -1,5 +1,44 @@
 package com.atcs.ecommerce.serviceimpl;
 
-public class OrderServiceImpl {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.atcs.ecommerce.entity.Order;
+import com.atcs.ecommerce.repo.OrderRepo;
+import com.atcs.ecommerce.service.OrderService;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+	@Autowired
+	OrderRepo repo;
+	
+	@Override
+	public void post(Order or) {
+		repo.save(or);
+		
+	}
+
+	@Override
+	public void put(Order or) {
+		repo.save(or);
+		
+	}
+
+	@Override
+	public Order getById(int id) {
+		return repo.findById(id).get();
+	}
+
+	@Override
+	public List<Order> getAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void delete(int id) {
+		repo.deleteById(id);
+	}
 
 }

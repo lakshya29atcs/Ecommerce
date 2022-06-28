ecdbcdf48d5c3d162e30acc261ac7af85d32c393
Lakshya Dewani <lakshya.dewani@atcs.com>
2022-06-28 17:41:10 +0530

    cart and service

--- EcommerceWeb/src/main/java/com/atcs/ecommerce/service/CartService.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/service/CartService.java
@@ -1,5 +1,23 @@
 package com.atcs.ecommerce.service;
 
-public class CartService {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.atcs.ecommerce.entity.Cart;
+
+
+@Service
+public interface CartService {
+	
+	public List<Cart> getData();
+
+	public void postData(Cart car);
+
+	public void putData(Cart car);
+
+	public void deleteData(int id);
+
+	public Cart getDataById(int id);
 
 }
--- EcommerceWeb/src/main/java/com/atcs/ecommerce/serviceimpl/ProductServiceImpl.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/serviceimpl/ProductServiceImpl.java
@@ -1,5 +1,41 @@
 package com.atcs.ecommerce.serviceimpl;
 
-public class ProductServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.atcs.ecommerce.entity.Product;
+import com.atcs.ecommerce.repo.ProductRepo;
+import com.atcs.ecommerce.service.ProductService;
+
+@Component
+public class ProductServiceImpl implements ProductService{
+	@Autowired
+	ProductRepo productRepo;
+	
+	@Override
+	public List<Product> getData() {
+		return productRepo.findAll();
+	}
+	
+	@Override
+	public void postData(Product pro) {
+		productRepo.save(pro);
+	}
+	@Override
+	public void putData(Product pro) {
+		productRepo.save(pro);
+	}
+	@Override
+	public void deleteData(int id) {
+		productRepo.deleteById(id);
+	}
+
+	@Override
+	public Product getDataById(int id) {
+		return productRepo.findById(id).get();
+	}
+
 
 }

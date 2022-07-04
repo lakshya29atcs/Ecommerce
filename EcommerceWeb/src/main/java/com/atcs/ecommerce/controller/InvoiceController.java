b17a22b0db2203c4eb20fdf00548c2ba1edd514a
dannyAtcs <danny.gupta@atcs.com>
2022-07-04 15:28:26 +0530

    Invoice Entity

---
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/controller/InvoiceController.java
@@ -0,0 +1,24 @@
+package com.atcs.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atcs.ecommerce.entity.Invoice;
+import com.atcs.ecommerce.service.InvoiceService;
+
+@RestController
+@RequestMapping("/invoice")
+public class InvoiceController {
+	
+	@Autowired
+	InvoiceService invoiceService;
+
+	@GetMapping("/{id}")
+	public Invoice getInvoice(@PathVariable("id") int id)
+	{
+		return invoiceService.getInvoice(id);
+	}
+}
--- EcommerceWeb/src/main/java/com/atcs/ecommerce/entity/Order.java
+++ EcommerceWeb/src/main/java/com/atcs/ecommerce/entity/Order.java
@@ -13,6 +13,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name="orders")
 public class Order {
@@ -70,6 +72,7 @@ public class Order {
 		this.totalPrice = totalPrice;
 	}
 
+	@JsonManagedReference
 	public List<OrderItem> getOrderItem() {
 		return orderItem;
 	}
